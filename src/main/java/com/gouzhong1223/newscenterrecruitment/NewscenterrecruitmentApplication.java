096288b77c232a8eb6ff312b2397e81ee90a3dc3
Gouzhong1223 <1162864960@qq.com>
2020-09-11 19:21:02 +0800

    初始化项目----init

---
+++ src/main/java/com/gouzhong1223/newscenterrecruitment/NewscenterrecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.gouzhong1223.newscenterrecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewscenterrecruitmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewscenterrecruitmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gouzhong1223/newscenterrecruitment/NewscenterrecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouzhong1223.newscenterrecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewscenterrecruitmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
