393f26ca1d9f319c5a32749f6e985f52da6a831e
Gouzhong1223 <1162864960@qq.com>
2020-09-17 22:01:29 +0800

    完善手机号和学号验证

--- src/main/java/com/gouzhong1223/newscenterrecruitment/services/impl/DataServiceImpl.java
+++ src/main/java/com/gouzhong1223/newscenterrecruitment/services/impl/DataServiceImpl.java
@@ -7,6 +7,7 @@ import com.gouzhong1223.newscenterrecruitment.mapper.StudentMapper;
 import com.gouzhong1223.newscenterrecruitment.services.DataService;
 import com.gouzhong1223.newscenterrecruitment.services.MailService;
 import com.gouzhong1223.newscenterrecruitment.utils.CheakEmail;
+import com.gouzhong1223.newscenterrecruitment.utils.SmsUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -26,6 +27,7 @@ public class DataServiceImpl implements DataService {
 
     private final StudentMapper studentMapper;
     private final MailService mailService;
+    private static final int STUDENT_ID_LENGTH = 10;
 
     public DataServiceImpl(StudentMapper studentMapper, MailService mailService) {
         this.studentMapper = studentMapper;
@@ -35,25 +37,41 @@ public class DataServiceImpl implements DataService {
     @Override
     public ResponseDto pushData(Student student) {
 
-        if (CheakEmail.isEmail(student.getEmail())) {
-            Student record = studentMapper.selectByPrimaryKey(student.getStudentId());
-            if (record != null) {
-                studentMapper.updateByPrimaryKeySelective(student);
-                mailService.sendSimpleMail(student.getEmail(), "【重邮新闻中心】- 您已成功修改报名信息！", "亲爱的" + student.getStudentName() + "同学:\n" +
-                        "您已成功修改您的报名信息！\n");
-                return new ResponseDto(ResultCode.SUCCESS.getCode(), "修改报名信息成功！", student);
-            } else {
-                try {
-                    studentMapper.insertSelective(student);
-                    mailService.sendSimpleMail(student.getEmail(), "【重邮新闻中心】- 您已报名成功！", "亲爱的" + student.getStudentName() + "同学:\n" +
-                            "我们已经收到您的意向部门信息，后续通知我们会通过 QQ 群的形式传播，请持续关注，谢谢！\n");
-                    return ResponseDto.SUCCESS(student);
-                } catch (Exception e) {
-                    return ResponseDto.FAIL();
-                }
+        // 验证学号
+        if (String.valueOf(student.getStudentId()).length() != STUDENT_ID_LENGTH) {
+            return new ResponseDto(ResultCode.FAIL.getCode(), "请填写正确的学号！");
+        }
+
+        // 验证手机号
+        if (!SmsUtils.isMobile(student.getPhone())) {
+            return new ResponseDto(ResultCode.FAIL.getCode(), "请填写正确的手机号！");
+        }
+
+        // 验证邮箱
+        if (!CheakEmail.isEmail(student.getEmail())) {
+            return new ResponseDto(ResultCode.FAIL.getCode(), "请填写正确的邮箱");
+        }
+
+        // 根据学号先去数据库查
+        Student record = studentMapper.selectByPrimaryKey(student.getStudentId());
+
+        if (record != null) {
+            // 说明已经报过名，现在是修改报名信息的逻辑
+            studentMapper.updateByPrimaryKeySelective(student);
+            mailService.sendSimpleMail(student.getEmail(), "【重邮新闻中心】- 您已成功修改报名信息！", "亲爱的" + student.getStudentName() + "同学:\n" +
+                    "您已成功修改您的报名信息！\n");
+            return new ResponseDto(ResultCode.SUCCESS.getCode(), "修改报名信息成功！", student);
+        } else {
+            // 这是首次报名
+            try {
+                studentMapper.insertSelective(student);
+                mailService.sendSimpleMail(student.getEmail(), "【重邮新闻中心】- 您已报名成功！", "亲爱的" + student.getStudentName() + "同学:\n" +
+                        "我们已经收到您的意向部门信息，后续通知我们会通过 QQ 群的形式传播，请持续关注，谢谢！\n");
+                return ResponseDto.SUCCESS(student);
+            } catch (Exception e) {
+                return ResponseDto.FAIL();
             }
         }
-        return new ResponseDto(ResultCode.FAIL.getCode(), "请填写正确的邮箱");
     }
 
 }
---
+++ src/main/java/com/gouzhong1223/newscenterrecruitment/utils/SmsUtils.java
@@ -0,0 +1,34 @@
+package com.gouzhong1223.newscenterrecruitment.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Pattern;
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.gouzhong1223.com
+ * @Description :
+ * @Date : create by QingSong in 2020-09-17 21:46
+ * @Email : gouzhong1223@gmail.com
+ * @Since : JDK 1.8
+ * @PackageName : com.gouzhong1223.newscenterrecruitment.utils
+ * @ProjectName : newscenterrecruitment
+ * @Version : 1.0.0
+ */
+public class SmsUtils {
+
+    private static final String REGEX_MOBILE = "((\\+86|0086)?\\s*)((134[0-8]\\d{7})|(((13([0-3]|[5-9]))|(14[5-9])|15([0-3]|[5-9])|(16(2|[5-7]))|17([0-3]|[5-8])|18[0-9]|19(1|[8-9]))\\d{8})|(14(0|1|4)0\\d{7})|(1740([0-5]|[6-9]|[10-12])\\d{7}))";
+
+    /**
+     * 校验手机号
+     *
+     * @param phone 手机号
+     * @return boolean true:是  false:否
+     */
+    public static boolean isMobile(String phone) {
+        if (StringUtils.isEmpty(phone)) {
+            return false;
+        }
+        return Pattern.matches(REGEX_MOBILE, phone);
+    }
+}
