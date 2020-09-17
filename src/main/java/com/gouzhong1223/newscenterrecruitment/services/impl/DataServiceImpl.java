d0af609fc112e88530f88944a397dd399b9271af
Gouzhong1223 <1162864960@qq.com>
2020-09-17 08:34:36 +0800

    完善邮箱验证

--- src/main/java/com/gouzhong1223/newscenterrecruitment/services/impl/DataServiceImpl.java
+++ src/main/java/com/gouzhong1223/newscenterrecruitment/services/impl/DataServiceImpl.java
@@ -6,6 +6,7 @@ import com.gouzhong1223.newscenterrecruitment.entity.Student;
 import com.gouzhong1223.newscenterrecruitment.mapper.StudentMapper;
 import com.gouzhong1223.newscenterrecruitment.services.DataService;
 import com.gouzhong1223.newscenterrecruitment.services.MailService;
+import com.gouzhong1223.newscenterrecruitment.utils.CheakEmail;
 import org.springframework.stereotype.Service;
 
 /**
@@ -33,20 +34,26 @@ public class DataServiceImpl implements DataService {
 
     @Override
     public ResponseDto pushData(Student student) {
-        Student record = studentMapper.selectByPrimaryKey(student.getStudentId());
-        if (record != null) {
-            studentMapper.updateByPrimaryKeySelective(student);
-            mailService.sendSimpleMail(student.getEmail(), "subject", student.getStudentName() + "修改成功！");
-            return new ResponseDto(ResultCode.SUCCESS.getCode(), "修改报名信息成功！", student);
-        } else {
-            try {
-                studentMapper.insertSelective(student);
-                mailService.sendSimpleMail(student.getEmail(), "subject", student.getStudentName());
-                return ResponseDto.SUCCESS(student);
-            } catch (Exception e) {
-                return ResponseDto.FAIL();
+
+        if (CheakEmail.isEmail(student.getEmail())) {
+            Student record = studentMapper.selectByPrimaryKey(student.getStudentId());
+            if (record != null) {
+                studentMapper.updateByPrimaryKeySelective(student);
+                mailService.sendSimpleMail(student.getEmail(), "【重邮新闻中心】- 您已成功修改报名信息！", "亲爱的" + student.getStudentName() + "同学:\n" +
+                        "您已成功修改您的报名信息！\n");
+                return new ResponseDto(ResultCode.SUCCESS.getCode(), "修改报名信息成功！", student);
+            } else {
+                try {
+                    studentMapper.insertSelective(student);
+                    mailService.sendSimpleMail(student.getEmail(), "【重邮新闻中心】- 您已报名成功！", "亲爱的" + student.getStudentName() + "同学:\n" +
+                            "我们已经收到您的意向部门信息，后续通知我们会通过 QQ 群的形式传播，请持续关注，谢谢！\n");
+                    return ResponseDto.SUCCESS(student);
+                } catch (Exception e) {
+                    return ResponseDto.FAIL();
+                }
             }
         }
+        return new ResponseDto(ResultCode.FAIL.getCode(), "请填写正确的邮箱");
     }
 
 }
